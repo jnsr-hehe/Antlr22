3de846ef394208ce589be9e38073b25b76d0c249
Markus Eble <markus.eble@online.de>
2022-06-06 22:32:57 +0200

    Antlr Hello World

---
+++ compiler/HelloWorldListener.java
@@ -0,0 +1,20 @@
+// Generated from HelloWorld.g4 by ANTLR 4.7.2
+package compiler;
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link HelloWorldParser}.
+ */
+public interface HelloWorldListener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link HelloWorldParser#greeting}.
+	 * @param ctx the parse tree
+	 */
+	void enterGreeting(HelloWorldParser.GreetingContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HelloWorldParser#greeting}.
+	 * @param ctx the parse tree
+	 */
+	void exitGreeting(HelloWorldParser.GreetingContext ctx);
+}
